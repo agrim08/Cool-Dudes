b492489ab2ad3b40f45841730a8d48411fcb16dc
agrim08 <agrimgupta0805@gmail.com>
2024-11-17 15:42:15 +0530

    made frontend using react

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import CreateUserModal from "./CreateUserModal";
+
+const Navbar = ({ setUsers }) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"900px"}>
+      <Box
+        px={4}
+        my={4}
+        borderRadius={5}
+        bg={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
+          {/* Left side */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={3}
+            display={{ base: "none", sm: "flex" }}
+          >
+            <img
+              src="https://img.freepik.com/premium-psd/neon-lines-globe-aqua-blue-circular-neon-lines-compass-decorations-circ-y2k-shapes-psd-gradients_1020495-85601.jpg"
+              width={50}
+              height={50}
+            />
+          </Flex>
+          {/* Right side */}
+          <Flex gap={3} alignItems={"center"}>
+            <Text
+              fontSize={"lg"}
+              fontWeight={500}
+              display={{ base: "none", md: "block" }}
+            >
+              Cool Dudes 😎
+            </Text>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+            </Button>
+            <CreateUserModal setUsers={setUsers} />
+          </Flex>
+        </Flex>
+      </Box>
+    </Container>
+  );
+};
+export default Navbar;
