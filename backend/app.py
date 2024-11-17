2fadf3883fe898586f936403dfeff34db41441ea
agrim08 <agrimgupta0805@gmail.com>
2024-11-17 16:28:48 +0530

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask,send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
@@ -11,7 +10,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db =  SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 fronted_folder = os.path.join(os.getcwd(),"..","frontend" )
 dist_folder = os.path.join(fronted_folder,"dist")
