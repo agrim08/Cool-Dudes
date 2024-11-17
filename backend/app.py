758000cb931f2cd0d512190555f2d78e5c8209e6
agrim08 <agrimgupta0805@gmail.com>
2024-11-17 15:54:33 +0530

    ready to deplo

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +12,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db =  SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+fronted_folder = os.path.join(os.getcwd(),"..","frontend" )
+dist_folder = os.path.join(fronted_folder,"dist")
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = 'index.html'
+    return send_from_directory(dist_folder,filename)
 
 import routes
 
